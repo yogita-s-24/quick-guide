7bdba6231dfc93c16d0e9776f2a1f83914cc47d3
Yogita Shete <yogitashete24@gmail.com>
2023-12-10 14:21:56 +0530

    ref : card styling

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -6,8 +6,8 @@ function Background() {
       <div className="absolute top-[5%] w-full py-10  flex justify-center text-zinc-600 text-xl font-semibold">
         Document
       </div>
-      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-semibold text-zinc-900">
-        Docs.
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[9.5vw] leading-none tracking-tighter font-semibold text-zinc-400">
+        Speedy Docs.
       </h1>
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,17 +4,17 @@ import { LuDownload } from "react-icons/lu";
 
 function Card() {
   return (
-      <div className="relative w-60 h-72 rounded-[50px] bg-blue-100/90 text-black px-8 py-10 overflow-hidden">
+      <div className="relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
         <FaRegFileAlt />
         <p className="text-sm mt-5 leading-tight font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         <div className="footer absolute bottom-0 left-0 w-full">
           <div className="flex items-center justify-between px-8 py-3 mb-5">
             <h5>.4mb</h5>
-            <span className="w-7 h-7 bg-sky-200 rounded-full flex items-center justify-center">
+            <span className="w-7 h-7 bg-zinc-200 rounded-full flex items-center justify-center">
               <LuDownload size=".7em" color="#000" />
             </span>
           </div>
-          <div className="tag w-full py-3 bg-sky-400 flex items-center justify-center">
+          <div className="tag w-full py-3 bg-red-600 flex items-center justify-center">
             <h3 className="text-sm font-semibold">Download Now</h3>
 
           </div>

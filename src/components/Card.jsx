0b900ffa013f2399f9ee391e4909f2bbc12b955d
Yogita Shete <yogitashete24@gmail.com>
2023-12-10 15:16:13 +0530

    feat(Foreground): add Card component for displaying background info

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
 
-function Card() {
+function Card({ data }) {
   return (
-      <div className="relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
-        <FaRegFileAlt />
-        <p className="text-sm mt-5 leading-tight font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <div className="footer absolute bottom-0 left-0 w-full">
-          <div className="flex items-center justify-between px-8 py-3 mb-5">
-            <h5>.4mb</h5>
-            <span className="w-7 h-7 bg-zinc-200 rounded-full flex items-center justify-center">
+    <div className="relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
+      <FaRegFileAlt />
+      <p className="text-sm mt-5 leading-tight font-semibold">{data.name}</p>
+      <div className="footer absolute bottom-0 left-0 w-full">
+        <div className="flex items-center justify-between px-8 py-3 mb-3">
+          <h5>{data.filesize}</h5>
+          <span className="w-7 h-7 bg-zinc-200 rounded-full flex items-center justify-center">
+            {data.close ? (
+              <IoClose color="#000" />
+            ) : (
               <LuDownload size=".7em" color="#000" />
-            </span>
-          </div>
+            )}
+          </span>
+        </div>
+
+        {data.tag.isOpen ? (
           <div className="tag w-full py-3 bg-red-600 flex items-center justify-center">
             <h3 className="text-sm font-semibold">Download Now</h3>
-
           </div>
-        </div>
+        ) : null}
       </div>
+    </div>
   );
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 
 function Foreground() {
-  return (
-    <div>
-              <div className='fixed top-0 left-0 z-[3] w-full h-full bg-white-900/50'>
-                <Card/>
-              </div>
+const data = [
+    {
+      name: "This is the background color that will be displayed.",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen:true, tagTitle: "Download Now", tagColor : "green" },
+    },
+  ];
 
-    </div>
-  )
+  return (
+      <div className="fixed top-0 left-0 z-[3] w-full h-full">
+        {data.map((item, index)=>(
+            <Card data={item}/>
+        ))}
+      
+      </div>
+    
+  );
 }
 
-export default Foreground
+export default Foreground;
